3c8698cc3ef917b6ba9dc0b44de4869a6398a259
Nikhil <nikhilanantharaman@gmail.com>
2024-07-07 23:09:17 -0700

    Revisions

--- chapter_1/ex10&11_fetchall/main.go
+++ chapter_1/ex10&11_fetchall/main.go
@@ -18,6 +18,16 @@ func main() {
 		fmt.Println(<-ch) // receive from channel ch
 	}
 	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+
+	start = time.Now()
+	ch = make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // start a goroutine
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // receive from channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
 }
 
 func fetch(url string, ch chan<- string) {
